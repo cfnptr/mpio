ebdee7bdb6b68ab45b38de25547d0970c196733e
Nikita Fediuchin <cfnptr@gmail.com>
2024-10-27 22:40:41 +0200

    Update docs

--- docs/os_8h.js
+++ docs/os_8h.js
@@ -1,7 +1,9 @@
 var os_8h =
 [
     [ "getCurrentClock", "os_8h.html#aa8dadbf5401d2d80893740fdb87961cf", null ],
-    [ "getCpuCount", "os_8h.html#ac7e7eebb4772b4a12fc2b775f6cc9071", null ],
+    [ "getLogicalCpuCount", "os_8h.html#aaefae302e63db549c3f989e3bbec47f7", null ],
+    [ "getPhysicalCpuCount", "os_8h.html#a793ae5b41f8d41fb29f641c5c5698283", null ],
+    [ "getPerformanceCpuCount", "os_8h.html#ad33c9518573b5bbe36739e28c6d11632", null ],
     [ "getTotalRamSize", "os_8h.html#a3e09009ef29e2ff41458d9ea0e26a0cc", null ],
     [ "getFreeRamSize", "os_8h.html#aa7ffb4dde2f51d9b4f8ca4d22c8f6aba", null ],
     [ "getCpuName", "os_8h.html#a9c51eff74e6dbae939c893f3e7567bfd", null ]
